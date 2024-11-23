11f17b823dc6eb75e84648aacd78c78817af165f
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2024-11-23 13:35:54 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchFormInput = document.querySelector("#search-form-input");
-  let cityElement = documet.querySelector("#city");
-  cityElement.innerHTML = searchFormInput.value;
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
 }
 
 let searchFormElement = document.querySelector("#search-form");
