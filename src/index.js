be694b6f85d3a3de1ee1f16916869b96d205d5a9
FatimadelaIglesia <fatimaiglesiav@gmail.com>
2024-11-24 13:33:53 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,14 +8,15 @@ function refreshWeather(response) {
 
 function searchCity(city) {
   let apiKey = "0d877aa762b4511ceo3e2a4a1bta01f6";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`; 
-  axios.get(apiUrl).then(refreshWeather); 
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
 
-  searchCity = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
